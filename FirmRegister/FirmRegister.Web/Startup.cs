5483368953c02d8770178e3cf2d888f9330598d8
Ivan <ivan.mladenov@abbaty.int>
2016-09-16 09:56:13 +0300

    Create projects

---
+++ FirmRegister/FirmRegister.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirmRegister.Web.Startup))]
+namespace FirmRegister.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
